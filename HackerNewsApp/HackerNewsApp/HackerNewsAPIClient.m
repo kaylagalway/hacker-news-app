c357046637c5147647f72764c87d0c8e870de83a
kaylagalway <kayla.galway@gmail.com>
2016-03-08 20:09:35 -0500

    passed array to completion block

--- HackerNewsApp/HackerNewsApp/HackerNewsAPIClient.m
+++ HackerNewsApp/HackerNewsApp/HackerNewsAPIClient.m
@@ -13,8 +13,12 @@
 + (void)fetchTopFiveHundredStoryIDs: (void (^)(NSArray *storyIDs))completion {
   NSURL *url = [NSURL URLWithString:@"https://hacker-news.firebaseio.com/v0/topstories.json"];
   
-  
+    //this is the same as creating an NSURLsession variable, creates a data task, and resumes session
   [[[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+      
+      NSArray *arrayWithID = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
+      
+      completion (arrayWithID);
     
     NSLog(@"%@", data);
     
