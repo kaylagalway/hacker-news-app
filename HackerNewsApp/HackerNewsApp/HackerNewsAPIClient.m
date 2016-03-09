f12f76b8535d135adc3b2ff183c1bd1be9ccb220
kaylagalway <kayla.galway@gmail.com>
2016-03-08 20:15:03 -0500

    comments on methods

--- HackerNewsApp/HackerNewsApp/HackerNewsAPIClient.m
+++ HackerNewsApp/HackerNewsApp/HackerNewsAPIClient.m
@@ -16,8 +16,10 @@
     //this is the same as creating an NSURLsession variable, creates a data task, and resumes session
   [[[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
       
+      //created a variable that contains the serialized data from the request which is turned into an NSObject(array in this case)
       NSArray *arrayWithID = [NSJSONSerialization JSONObjectWithData:data options:0 error:nil];
       
+      //called completion and passed it arrayWithID
       completion (arrayWithID);
     
     NSLog(@"%@", data);
