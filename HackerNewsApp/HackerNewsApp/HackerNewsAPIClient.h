e802dc16b00c4f043b88fc70b63276e2a72b82c1
Dare <dryan@scrippsnetworks.com>
2016-03-08 19:32:37 -0500

    Create new API client class

---
+++ HackerNewsApp/HackerNewsApp/HackerNewsAPIClient.h
@@ -0,0 +1,13 @@
+//
+//  HackerNewsAPIClient.h
+//  HackerNewsApp
+//
+//  Created by Ryan, Dare on 3/8/16.
+//  Copyright © 2016 edu.self. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HackerNewsAPIClient : NSObject
+
+@end
---
+++ HackerNewsApp/HackerNewsApp/HackerNewsAPIClient.m
@@ -0,0 +1,13 @@
+//
+//  HackerNewsAPIClient.m
+//  HackerNewsApp
+//
+//  Created by Ryan, Dare on 3/8/16.
+//  Copyright © 2016 edu.self. All rights reserved.
+//
+
+#import "HackerNewsAPIClient.h"
+
+@implementation HackerNewsAPIClient
+
+@end
