5cbd9f59a1f7f8c1607760b2ffbb076b6428bb5a
kaylagalway <kayla.galway@gmail.com>
2016-03-08 19:29:38 -0500

    firstweathercommit

---
+++ HackerNewsApp/HackerNewsApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HackerNewsApp
+//
+//  Created by Kayla Galway on 3/8/16.
+//  Copyright © 2016 edu.self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HackerNewsApp/HackerNewsApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HackerNewsApp
+//
+//  Created by Kayla Galway on 3/8/16.
+//  Copyright © 2016 edu.self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
